75a441c3ec7ddd2b2094cdb4e72b4c3ce0b75ba1
Arvind A <pingarvindforquestions@gmail.com>
2025-02-15 17:32:18 +0530

    fix: allowed host

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = DEBUG
 
-ALLOWED_HOSTS = ["*.vercel.app", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
