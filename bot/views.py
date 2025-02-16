586c6bfaa5fa344e8894462352d68bc0f7df2a0b
Arvind A <pingarvindforquestions@gmail.com>
2025-02-16 15:50:26 +0530

    fix: added sample qs in home route

--- bot/views.py
+++ bot/views.py
@@ -1,5 +1,6 @@
 import json
 from bot.services.slack import app
+from bot.models import Conversation
 from django.views.decorators.csrf import csrf_exempt
 from slack_bolt.adapter.django import SlackRequestHandler
 from django.http import (HttpResponse, HttpRequest, JsonResponse,)
@@ -7,6 +8,8 @@ from django.http import (HttpResponse, HttpRequest, JsonResponse,)
 handler = SlackRequestHandler(app=app)
 
 def hello(request: HttpRequest) -> HttpResponse:
+    qs = Conversation.objects.all()
+    print(qs)
     return JsonResponse({"ping": "pong"})
 
 @csrf_exempt
