9a5141d11a765363bbd2fc216096122bd7a9cbf4
Arvind A <pingarvindforquestions@gmail.com>
2025-02-16 15:31:17 +0530

    feat: updated the ai feature

--- bot/services/ai.py
+++ bot/services/ai.py
@@ -1,17 +1,24 @@
-# from openai import OpenAI
-# from decouple import config
+import requests
+from backend.constants import (OPEN_ROUTER_API_KEY, BASE_URL,)
 
-# client = OpenAI(api_key=config('OPENAI_API_KEY'))
 
-def get_openai_response(conversation_history):
+def get_ai_response(conversation_history) -> str:
+    data = {
+        'model': 'deepseek/deepseek-r1:free',
+        'messages': [{
+            'role': 'user',
+            'content': f'Draft a response to the message and I have attached the following message history context:\n{conversation_history}\n. Write a response that is concise and to the point. Do not add any extra information. If the message is not related to the context, respond with "I am sorry, I do not understand the message". Do not add any extra information. If the message is not related to the context, respond with "I am sorry, I do not understand the message". Do not add any extra information. If the message is not related to the context, respond with "I am sorry, I do not understand the message".',
+        }],
+    }
     try:
-        # response = client.chat.completions.create(
-        #     model="gpt-4",
-        #     messages=conversation_history,
-        #     temperature=0.7,
-        #     max_tokens=1500
-        # )
-        # return response.choices[0].message.content
-        pass
+        headers = {
+            'Authorization': f'Bearer {OPEN_ROUTER_API_KEY}',
+            'Content-Type': 'application/json',
+        }
+        response = requests.post(BASE_URL, json=data, headers=headers)
+        response.raise_for_status()
+        response_data = response.json()
+        return response_data['choices'][0]['message']['content']
     except Exception as e:
-        return f"Sorry, I encountered an error: {str(e)}"
+        print(e)
+        return f"Sorry, I encountered an error. Please try again later."
